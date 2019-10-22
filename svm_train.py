744882b90c4ee57a9fdb0355bb7046096fc2b3ea
bigkeios <cqphuong97@gmail.com>
2019-10-22 18:28:05 +0700

    Fit model to data trimmed records too short

--- svm_train.py
+++ svm_train.py
@@ -4,6 +4,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import pickle
 import tensorflow as tf
+import numpy as np
 
 def build_train_data(dir):
   """ Build training data array
@@ -30,15 +31,16 @@ def main():
   # Load data
   dir = "../audioset/train/"
   data, label = build_train_data(dir)
+  # print(label)
   train_data, eval_data, train_label, eval_label = train_test_split(data, label,
   test_size=0.2, random_state=1)
   # create and train the model
   svm_model = SVC(kernel = 'linear', C=1e5)
   svm_model.fit(train_data, train_label)
   # calculate accuracy
-  predicted_label = svm_model.predict(train_label)
+  predicted_label = svm_model.predict(eval_data)
   print(predicted_label)
-  # print("Accuarcy score: {0:.2f} %".format(100 * svm_model.score(eval_data, eval_label)))
+  print("Accuracy score: {0:.2f}%".format(100 * svm_model.score(eval_data, eval_label)))
   # save model trained
   model_filename = "svm_model.pkl"
   with open(model_filename, "wb") as file:
