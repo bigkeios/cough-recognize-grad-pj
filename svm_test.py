7cdfd8e9c67a20525f3b8388479966a9ec568a67
bigkeios <cqphuong97@gmail.com>
2019-12-24 14:00:54 +0700

    Update old code mixed in

--- svm_test.py
+++ svm_test.py
@@ -1,6 +1,6 @@
 from read_tfrecord import build_train_data
 from sklearn import preprocessing
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 import tensorflow as tf
 import pickle
 
@@ -8,13 +8,17 @@ def main():
   tf.compat.v1.enable_eager_execution()
   print("Building test data...")
   # Directory of test file(s). Is to be changed
-  dir_test = "../audioset/test/"
+  dir_test = "../Test not in training/iso/"
   test_data, test_label = build_train_data(dir_test)
-  test_data_scaled = preprocessing.scale(test_data)
+  test_data_scaled = preprocessing.normalize(test_data)
   print("Predicting... ")
-  svm, svm_coef = pickle.load(open('svm_model.pkl', 'rb'))
+  svm = pickle.load(open('svm_mix.pkl', 'rb'))
   test_predict = svm.predict(test_data_scaled)
   # print test_predict to show raw results
+  print("Raw result:")
+  print(test_predict)
+  print("Confusion matrix:")
+  print(confusion_matrix(test_label, test_predict))
   print("Classification report: ")
   print(classification_report(test_label, test_predict))
 
